d8ea9a88d9c76f3b56c01b1cf270646f54830b20
mahgolmot <mahgol.motaqian1384@gmail.com>
2026-06-10 22:10:26 +0330

    feat: implement carousel slider for hero section

--- js/home.js
+++ js/home.js
@@ -1,12 +1,22 @@
-const navMenu = document.getElementById("nav-menu");
-const navToggle = document.getElementById("nav-toggle");
-const navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
 //header and navbar
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("show-menu");
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
   //   navMenu.classList.add("nav-logo-open");
 });
-navClose.addEventListener("click", () => {
-  navMenu.classList.remove("show-menu");
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+});
+
+// carousel-slider
+
+const slider = document.getElementById('slider');
+
+document.addEventListener('click', (e) => {
+  const items = document.querySelectorAll('.slider-item');
+  e.target.matches('.next') && slider.append(items[0]);
+  e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
 });
